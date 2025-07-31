091cd58a758847d8b2900f0278694509a587d6ae
Ayush <rajeevdu972@gmail.com>
2025-08-01 04:07:39 +0530

    fixed types error in cart-actions

--- projects/amazon/src/actions/cart-actions.ts
+++ projects/amazon/src/actions/cart-actions.ts
@@ -2,9 +2,7 @@
 
 import db from "@/services/Prisma";
 import { getCurrentUser } from "./auth-actions";
-import { productData } from "@/data/data";
-import { title } from "process";
-import { CartItem } from "@prisma/client";
+
 
 type cartItem = {
   productId: number;
@@ -90,7 +88,7 @@ export async function getUserCart() {
       return { success: false, message: "Unauthorized" };
     }
     const { cart, user } = res;
-    const items = cart.items as CartItem[];
+    const items = cart.items as cartItem[];
     const productIds = items.map((item) => item.productId);
     const products = await db.product.findMany({
       where: {
