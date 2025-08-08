ca6aced16a62cd85fe458835547a913cf7305828
Ayush <rajeevdu972@gmail.com>
2025-08-09 03:22:14 +0530

    fixed prisma client export

--- projects/job-search/src/lib/prisma.ts
+++ projects/job-search/src/lib/prisma.ts
@@ -1,4 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-const db = new PrismaClient();
+let db: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  db = new PrismaClient();
+} else {
+  // In dev, prevent multiple instances due to hot-reloading
+  if (!(global as any).prisma) {
+    (global as any).prisma = new PrismaClient();
+  }
+  db = (global as any).prisma;
+}
+
 export default db;
